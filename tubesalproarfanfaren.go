627226776a4f1c9b62e73d459353ee6d10f4f2c9
ArfanRM <arfan.mahzar@gmail.com>
2025-05-11 20:29:14 +0700

    Add files via upload

---
+++ tubesalproarfanfaren.go
@@ -0,0 +1,5 @@
+//Aplikasi ini membantu pengguna membuat resume dan surat lamaran kerja secara otomatis berdasarkan:
+// Input pengguna: keahlian, pengalaman, pendidikan, pekerjaan yang dilamar.
+// Algoritma AI: untuk memberikan saran penyusunan resume dan surat lamaran yang relevan.
+// Fitur pencarian: untuk menemukan posisi kerja berdasarkan kata kunci.
+// Fitur pengurutan: pekerjaan bisa diurut berdasarkan gaji atau relevansi.
